c02ef94bd15f72bd9ec20f5689658b3153fbf8c6
Muhammad Zamroni <halo@matriphe.com>
2021-03-13 23:04:43 +0100

    Remove unused TearDown

--- tests/UN/Locode/LocodeTest.php
+++ tests/UN/Locode/LocodeTest.php
@@ -28,14 +28,6 @@ class LocodeTest extends TestCase
     }
 
     /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown(): void
-    {
-    }
-
-    /**
      * @covers {className}::{origMethodName}
      */
     public function testGetListByCountry()
